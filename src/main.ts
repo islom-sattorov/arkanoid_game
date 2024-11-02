ca8c48ff441a091db56a67e470af1b9af1b7e698
islom-sattorov <slmsttrv1@gmail.com>
2024-11-02 23:29:59 +0500

    add: index

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+import PADDLE_IMAGE from "./images/paddle.png";
+import BALL_IMAGE from "./images/ball.png";
+import {
+  LEVEL,
+  PADDLE_SPEED,
+  PADDLE_WIDTH,
+  PADDLE_HEIGHT,
+  PADDLE_STARTX,
+  BALL_SIZE,
+  BALL_SPEED,
+  BALL_STARTX,
+  BALL_STARTY,
+} from "./setup";
+
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView) {
+  view.drawInfo("Game over!");
+  gameOver = false;
+}
+
+function setGameWin(view: CanvasView) {
+  view.drawInfo("Game Won!");
+  gameOver = false;
+}
+
+function gameLoop(
+  view: CanvasView,
+  bricks: Brick[],
+  paddle: Paddle,
+  ball: Ball,
+) {}
+
+function startGame(view: CanvasView) {}
+
+// Create a new view
+const view = new CanvasView("playField");
+view.initStartButton(startGame);
